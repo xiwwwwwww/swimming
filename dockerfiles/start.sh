02495185166f95d7c93ff44c38d96fd1706c4049
xiwwwwwww <qq19836135940101@163.com>
2026-07-18 07:25:27 +0800

    Add files via upload

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -2,16 +2,6 @@
 set -e
 
 if [[ $@ ]]; then
-    # Pre-clone and patch p4a so buildozer finds it already in place
-    mkdir -p .buildozer/android/platform
-    if [ ! -d ".buildozer/android/platform/python-for-android" ]; then
-        git clone --depth 1 --branch develop https://github.com/kivy/python-for-android.git .buildozer/android/platform/python-for-android
-        RECIPE=".buildozer/android/platform/python-for-android/pythonforandroid/recipes/libthorvg/__init__.py"
-        if [ -f "$RECIPE" ]; then
-            sed -i "s/clang_lib_dir = glob(pattern)\[0\]/clang_lib_dirs = glob(pattern); clang_lib_dir = clang_lib_dirs[0] if clang_lib_dirs else None/" "$RECIPE"
-            echo "Patched libthorvg"
-        fi
-    fi
     eval $@
 else
     /bin/bash
